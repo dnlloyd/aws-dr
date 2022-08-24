f326d9819aa8bd720e88f3c4801aa852d5a08fef
Daniel Lloyd <daniel.lloyd@foghornconsulting.com>
2022-08-24 14:29:10 -0500

    aws backup #

---
+++ dr/aws-backup-rds.tf
@@ -0,0 +1,17 @@
+resource "aws_kms_key" "backups" {
+  provider = aws.dr
+
+  description = "KMS key for AWS backups"
+  deletion_window_in_days = 10
+}
+
+resource "aws_backup_vault" "dr_region" {
+  provider = aws.dr
+
+  name = "dr-region"
+  kms_key_arn = aws_kms_key.backups.arn
+}
+
+output "aws_backup_vault" {
+  value = aws_backup_vault.dr_region
+}
--- dr/main.tf
+++ dr/main.tf
@@ -40,21 +40,3 @@ module "my_stack" {
   dr_event = local.dr_event
   primary_remote_state = data.terraform_remote_state.primary.outputs
 }
-
-resource "aws_kms_key" "backups" {
-  provider = aws.dr
-
-  description = "KMS key for AWS backups"
-  deletion_window_in_days = 10
-}
-
-resource "aws_backup_vault" "dr_region" {
-  provider = aws.dr
-
-  name = "dr-region"
-  kms_key_arn = aws_kms_key.backups.arn
-}
-
-output "aws_backup_vault" {
-  value = aws_backup_vault.dr_region
-}
