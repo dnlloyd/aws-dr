835873b36c3f553c7e02567e1314c6ad07121322
Daniel Lloyd <daniel.lloyd@foghornconsulting.com>
2022-08-24 09:17:47 -0500

    aws backup 2

--- dr/main.tf
+++ dr/main.tf
@@ -42,11 +42,15 @@ module "my_stack" {
 }
 
 resource "aws_kms_key" "backups" {
+  provider = aws.dr
+
   description = "KMS key for AWS backups"
   deletion_window_in_days = 10
 }
 
 resource "aws_backup_vault" "dr_region" {
+  provider = aws.dr
+  
   name = "dr-region"
   kms_key_arn = aws_kms_key.backups.arn
 }
