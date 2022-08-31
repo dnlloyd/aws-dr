d975eeb69d1601b3cb28563d1a9b9173a44e0a8b
Daniel Lloyd <daniel.lloyd@foghornconsulting.com>
2022-08-31 17:07:35 -0500

    RDS with AWS backup module

--- dr/main.tf
+++ dr/main.tf
@@ -21,7 +21,7 @@ locals {
 # Module RDS with AWS Backup
 
 module "rds_with_backups" {
-  source = "./modules/aws-backup-rds"
+  source = "./modules/rds-with-backup"
 }
 
 # Capture all the outputs from the module instantiation above
