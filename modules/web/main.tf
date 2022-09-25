3132b8b5ff944b42c73610f8ac2d7cf32cacd7ea
Daniel Lloyd <daniel.lloyd@foghornconsulting.com>
2022-09-25 15:09:28 -0500

    CloudFront DR

--- modules/web/main.tf
+++ modules/web/main.tf
@@ -44,6 +44,8 @@ resource "aws_route53_record" "acm_validation" {
 }
 
 resource "aws_acm_certificate_validation" "main" {
+  provider = aws.acm
+  
   certificate_arn = aws_acm_certificate.main.arn
   validation_record_fqdns = [for record in aws_route53_record.acm_validation : record.fqdn]
 }
